fe6813c8a18a2b51dc47c88dadb88be30de8d1d4
MrKapino <matthias.juentgen@gmail.com>
2023-06-06 22:39:45 +0200

    second commit, testing

---
+++ HPKampfUmHogwarts/src/main/Card.java
@@ -0,0 +1,5 @@
+package main;
+
+public interface Card {
+	public void use(Player player, Player[] others);
+}
---
+++ HPKampfUmHogwarts/src/main/Player.java
@@ -0,0 +1,8 @@
+package main;
+
+public class Player {
+	private Character character;
+	private Card[] hand;
+	private int lightning;
+	private int coin;
+}
